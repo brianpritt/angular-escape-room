fe1c0c124e6d42d46cd8229fd96241694b8de7e9
Brian Pritt and Erica Nafziger <brian+erica@>
2017-01-24 16:43:42 -0800

    finish studen/con routes, add boolens for items, can collect items and win

--- src/app/profile.model.ts
+++ src/app/profile.model.ts
@@ -1,4 +1,8 @@
 export class Profile {
-  public ohayocon: boolean = false; public scientist: boolean = false; public key: boolean = false;
+  public ohayocon: boolean = false;
+  public scientist: boolean = false;
+  public key: boolean = false;
+  public paperclip: boolean = false;
+  public password: boolean = false;
   constructor(public name: string, public character: string) {}
 }
--- src/app/question-details/question-details.component.ts
+++ src/app/question-details/question-details.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
-import { Question } from '../question.model';
 import { Router } from '@angular/router';
+import { Question } from '../question.model';
 import { QuestionService } from '../question.service';
-
+import { Profile } from '../profile.model';
+import { ProfileService } from '../profile.service';
 @Component({
   selector: 'app-question-details',
   templateUrl: './question-details.component.html',
   styleUrls: ['./question-details.component.css'],
-  providers: [QuestionService]
+  providers: [QuestionService, ProfileService]
 })
 export class QuestionDetailsComponent implements OnInit {
   questionId: number;
   questionToDisplay: Question;
+  userProfile: Profile[];
 
-  constructor(private router: Router, private route: ActivatedRoute, private location: Location, private questionService: QuestionService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private location: Location, private questionService: QuestionService, private profileService: ProfileService) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.questionId = parseInt(urlParameters['id']);
     });
+    this.userProfile = this.profileService.getProfiles();
     this.questionToDisplay = this.questionService.getQuestionById(this.questionId);
   }
   goToNewRoute(clickedRoute) {
@@ -28,7 +31,22 @@ export class QuestionDetailsComponent implements OnInit {
     this.route.params.forEach((urlParameters) => {
       this.questionId = parseInt(urlParameters['id']);
       this.questionToDisplay = this.questionService.getQuestionById(this.questionId);
+      console.log(this.userProfile);
+      if(this.questionId === 4){
+        this.userProfile[0].key = true;
+      }
+      if(this.questionId === 18){
+        this.userProfile[0].paperclip = true;
+      }
+      if(this.questionId === 14){
+        this.userProfile[0].password = true;
+      }
+      if(this.userProfile[0].key === true && this.userProfile[0].paperclip === true && this.userProfile[0].password === true) {
+        alert('Winner!');
+      }
     });
+
+
   }
 
 }
